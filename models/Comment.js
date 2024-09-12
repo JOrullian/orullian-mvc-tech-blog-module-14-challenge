6be542e1f784cabc057d88840b0a203037d3c646
JOrullian <jedediahorullian@gmail.com>
2024-09-12 14:19:07 -0500

    added new model and routes for comments.  Users can add and view comments on a blogPost.  Also added the main, blogPost, homepage, login and profile handlebars.

---
+++ models/Comment.js
@@ -0,0 +1,48 @@
+// models/Comment.js
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    blog_post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'blogpost',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -1,13 +1,25 @@
-const User = require('./User');
-const BlogPost = require('./BlogPost');
+const User = require("./User");
+const BlogPost = require("./BlogPost");
 
 User.hasMany(BlogPost, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
-BlogPost.belongsTo(User, {
-  foreignKey: 'user_id'
+BlogPost.belongsTo(User, { foreignKey: "user_id" });
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
+
+Comment.belongsTo(User, { foreignKey: "user_id " });
+
+BlogPost.hasMany(Comment, {
+  foreignKey: "blog_post_id",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(BlogPost, { foreignKey: "blog_post_id" });
 
 module.exports = { User, BlogPost };
