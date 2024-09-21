7ba692aa0fc09705b2fbfd148accf91da593176d
JOrullian <jedediahorullian@gmail.com>
2024-09-21 13:33:20 -0500

    reseeding render database to fix columns

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,36 +1,37 @@
-const sequelize = require('../config/connection');
-const { User, BlogPost } = require('../models');
+const sequelize = require("../config/connection");
+const { User, BlogPost } = require("../models");
 
-const userData = require('./userData.json');
-const blogPostData = require('./blogPostData.json');
+const userData = require("./userData.json");
+const blogPostData = require("./blogPostData.json");
 
 const seedDatabase = async () => {
-  try {
-    console.log('Synchronizing database...');
-    await sequelize.sync({ force: true });
+  // if (process.env.NODE_ENV !== "production") {
+    try {
+      await sequelize.sync({ force: true }); // Only in development
 
-    console.log('Seeding users...');
-    const users = await User.bulkCreate(userData, {
-      individualHooks: true,
-      returning: true,
-    });
-
-    console.log(users);
-
-    console.log('Seeding blog posts...');
-    for (const blogPost of blogPostData) {
-      await BlogPost.create({
-        ...blogPost,
-        user_id: users[Math.floor(Math.random() * users.length)].id,
+      console.log("Seeding users...");
+      const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
       });
-    }
 
-    console.log('Database seeded successfully.');
-  } catch (error) {
-    console.error('Error seeding database:', error);
-  } finally {
-    process.exit(0);
-  }
+      console.log("Seeding blog posts...");
+      for (const blogPost of blogPostData) {
+        await BlogPost.create({
+          ...blogPost,
+          user_id: users[Math.floor(Math.random() * users.length)].id,
+        });
+      }
+
+      console.log("Database seeded successfully.");
+    } catch (error) {
+      console.error("Error seeding database:", error);
+    } finally {
+      process.exit(0);
+    }
+  // } else {
+  //   console.log("Skipping seed in production.");
+  // }
 };
 
 seedDatabase();
